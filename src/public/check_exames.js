50943ba7afd1d0f3511c5238e2672c4db3cce029
wagnermarques <wagnerdocri@gmail.com>
2018-10-30 22:28:08 -0300

    adjust mixed content issue for works with heroku

--- src/public/check_exames.js
+++ src/public/check_exames.js
@@ -1,12 +1,12 @@
-//let cols = [];
-//let colModel = [];
+let cols = [];
+let colModel = [];
 $( document ).ready(function() {
     $.ajax({
-        url:"/select_all_exames",                 
+        url:"https://checkexames.herokuapp.com/select_all_exames",                 
         success: function(result){
             result.forEach((item)=>{
                 Object.keys(item).forEach((key)=>{
-                    //document.getElementById("divListaDeExames").innerHTML +="<p style='background-color:yellow'>"+ key +" = "+item[key]+"</p>";
+                    document.getElementById("divListaDeExames").innerHTML +="<p style='background-color:yellow'>"+ key +" = "+item[key]+"</p>";
                     cols.push(key);
                     let model = {
                         name:key,index:'id', width: "50%", sorttype:"int"
@@ -27,23 +27,3 @@ $( document ).ready(function() {
     });
     console.log(cols);
 });
-
-
-//jQuery("#jsonmap").jqGrid('navGrid','#pjmap',{edit:false,add:false,del:false});
-//;$( document ).ready(function() {
-//    $.ajax({
-//        url:"http://localhost:3000/select_all_exames",                 
-//        success: function(result){
-//            result.forEach((item)=>{
-//                document.getElementById("report-div").innerHTML += "<h2>"+item.nome+"</h2>";
-//                Object.keys(item).forEach((key)=>{
-//                    if(item[key]){
-//                        if(key != "id" && key != "nome"){
-//                            document.getElementById("report-div").innerHTML += "<p>"+key+"<p>";
-//                        }
-//                    }
-//                });        
-//            });//result.forEach((item)=>{
-//        }//success: function(result){
-//    });
-//});
